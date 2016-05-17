5ab1272cc32cb70d29b5d5547d08823353dd7dc4
joetainment <github@teaching3d.com>
2016-05-17 02:09:31 -0700

    edits

---
+++ git-add-commit-and-push-to-origin-master.js
@@ -0,0 +1,24 @@
+var child_process = require('child_process');//// This function will run if we answer 'y' to the promptfunction runGit( ){        cmds = []
+        cmds.push( 'git add --all' )
+        cmds.push( 'git commit --all -m "edits"');
+        cmds.push( 'git push origin master');
+
+        for ( i in cmds ){
+            child_process.execSync( cmds[i] );
+        }
+
+        console.log( 'done' );
+        console.log('Command-line input received:');
+        console.log('  Username: ' + result.username);
+        console.log('  Password: ' + result.password);}//// Warn the user and prompt in order to run the function!console.log( "About to add all, commit, and push to origin master!");
+console.log( "This is dangerous!");
+console.log( "Are you sure?" );process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var util = require('util');
+process.stdin.on('data', function (text) {
+    console.log('received data:', util.inspect(text));
+    if (text[0] === 'y') {
+      runGit();
+      process.exit();
+    }
+});
