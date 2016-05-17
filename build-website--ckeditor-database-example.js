fd3e8db062aa9bc899de6c4103c624f1164c1173
joetainment <github@teaching3d.com>
2016-05-16 22:59:03 -0700

    ckeditor breakup
    ckeditor

---
+++ build-website--ckeditor-database-example.js
@@ -0,0 +1,4 @@
+//// Includes via RequireJSvar ChildProcess = require('child_process');var Coffee = require('coffee-script');
+var entries =  mkdirsh.mkdir('-p', entry);    
+sh.cp( '-R', 'bower_components/ckeditor', 'js/lib/' );                                                                                  
+sh.cp( '-R', 'bower_components/classify/dist/*', 'js/lib/classify/' );                   
